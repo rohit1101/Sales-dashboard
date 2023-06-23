bf37611e4d947c35c3454aca95dc5b4bcac67fae
rohit1101 <rohit@Rohits-MacBook-Air.local>
2023-06-23 16:17:46 +0530

    set space-between

--- src/components/organisms/Topbar/Topbar.jsx
+++ src/components/organisms/Topbar/Topbar.jsx
@@ -8,42 +8,44 @@ import Down from "../../../assets/chevron-down.svg";
 
 function Topbar() {
   return (
-    <div className="p-5 flex flex-row items-center h-16 bg-white">
+    <div className="p-5 flex flex-row items-center h-16 bg-white sticky top-0 z-10 justify-between">
       <p className="font-bold text-2xl pr-56">Dashboard</p>
       <input
         type="text"
         placeholder="Search here..."
         className="w-96 py-2 px-9 rounded-md bg-[#f9fafc] bg-inp-image bg-no-repeat bg-[13px_center]"
       />
-      <div className="pl-20">
-        <NavItems
-          icon={USALogo}
-          borderRadius
-          text={
-            <p className="text-black flex flex-row gap-3 items-center text-sm font-bold">
-              Eng (US)
-              <span>
-                <img src={Downgray} alt="down" width="14px" height="14px" />
-              </span>
-            </p>
-          }
-          className="gap-2"
-        />
-      </div>
-      <div className="pl-8">
-        <div className="w-8 h-8 relative rounded-sm flex justify-center items-center bg-light-yellow">
-          <NavItems icon={Bell} alt="bell-icon" />
-          <div className="absolute w-1 h-1 rounded-full bg-red-500 top-[2px] right-[2px]" />
+      <div className="flex flex-row items-end">
+        <div className="pl-20">
+          <NavItems
+            icon={USALogo}
+            borderRadius
+            text={
+              <p className="text-black flex flex-row gap-3 items-center text-sm font-bold">
+                Eng (US)
+                <span>
+                  <img src={Downgray} alt="down" width="14px" height="14px" />
+                </span>
+              </p>
+            }
+            className="gap-2"
+          />
         </div>
-      </div>
-      <div className="pl-10 flex gap-3">
-        <img src={Profile} alt="profile-img" className="" />
-        <div className="flex gap-3">
-          <div>
-            <p className="font-light text-[15px]">Musfiq</p>
-            <p className="font-light text-sm text-gray-400">Admin</p>
+        <div className="pl-8">
+          <div className="w-8 h-8 relative rounded-sm flex justify-center items-center bg-light-yellow">
+            <NavItems icon={Bell} alt="bell-icon" />
+            <div className="absolute w-1 h-1 rounded-full bg-red-500 top-[2px] right-[2px]" />
           </div>
-          <img src={Down} alt="down" className="mt-1 self-start" />
+        </div>
+        <div className="pl-10 flex gap-3">
+          <img src={Profile} alt="profile-img" className="" />
+          <div className="flex gap-3">
+            <div>
+              <p className="font-light text-[15px]">Musfiq</p>
+              <p className="font-light text-sm text-gray-400">Admin</p>
+            </div>
+            <img src={Down} alt="down" className="mt-1 self-start" />
+          </div>
         </div>
       </div>
     </div>
